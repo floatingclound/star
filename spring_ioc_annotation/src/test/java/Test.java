6f43e9b05eaf12aff1e7d6d697749e77b56674c6
summer <957041293@qq.com>
2021-08-21 21:23:48 +0800

    repair

--- spring_ioc_annotation/src/test/java/Test.java
+++ spring_ioc_annotation/src/test/java/Test.java
@@ -1,10 +1,10 @@
 public class Test{
     public static void main(String[] args) {
         System.out.println("Hello world");
-<<<<<<< HEAD
+
         System.out.println("blue ");
-=======
+
         System.out.println("hello");
->>>>>>> 9b5261595d8c554a070f3c1358dfdf6fbc2a3eb0
+
     }
 }
